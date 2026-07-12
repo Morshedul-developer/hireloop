2b958c210515d068a544814e224a6d3598c5360f
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-07-12 22:32:05 +0600

    added a popular section description

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,9 @@
-
 const PopularSection = () => {
     return (
         <div>
             <h1>Popular Section</h1>
             <p>This is the popular section.</p>
+            <p>Rhetorical Mode: In literature and composition, description is one of the four core rhetorical modes (along with narration, exposition, and argumentation) used to paint a picture with words.</p>
         </div>
     );
 };
