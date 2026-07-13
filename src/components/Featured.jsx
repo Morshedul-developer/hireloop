9e0e666c9f68f3ef9855306a0df05ede18b59f70
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-07-13 22:07:17 +0600

    fixed some bug to the featured section

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,7 +3,7 @@ const FeaturedJobs = () => {
     return (
         <div>
             <h1>Featured Jobs</h1>
-            <p>Download my latest resume here to see my background in [Industry]. I am currently seeking [Target Role] roles where I can leverage my expertise in [Key Skill].</p>
+            <p>Download my latest resume here to see my background in Industry. I am currently seeking Target Role roles where I can leverage my expertise in Key Skill.</p>
         </div>
     );
 };
