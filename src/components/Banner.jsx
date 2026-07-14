9f349dc637d433024781a827b8919739322a35df
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-07-14 23:06:48 +0600

    added banner des

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ const Banner = () => {
     return (
         <div>
             <h1>Banner</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
     );
 };
