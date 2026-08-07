c66caa89dacfcc5c831cb76df1e1d907f1577bd8
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-08-07 23:10:07 +0600

    created the /help-center route page

---
+++ src/app/(site)/help-center/page.js
@@ -0,0 +1,11 @@
+import HelpCenter from "@/components/HelpCenter";
+
+export const metadata = {
+  title: "Help Center | HireLoop",
+  description:
+    "Find answers about your HireLoop account, applications, billing, and more.",
+};
+
+export default function HelpCenterPage() {
+  return <HelpCenter />;
+}
