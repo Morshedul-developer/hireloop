130501349f38c912b3b5bf34636673d731aa545e
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-08-10 23:01:52 +0600

    created the /contact route page

---
+++ src/app/(site)/contact/page.js
@@ -0,0 +1,11 @@
+import Contact from "@/components/Contact";
+
+export const metadata = {
+  title: "Contact Us | HireLoop",
+  description:
+    "Get in touch with the HireLoop team for support, sales, or press inquiries.",
+};
+
+export default function ContactPage() {
+  return <Contact />;
+}
