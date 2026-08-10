ca0aea4e185cc6565ec7fb666369971089aea529
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-08-10 22:59:02 +0600

    created the /career-library route page

---
+++ src/app/(site)/career-library/page.js
@@ -0,0 +1,11 @@
+import CareerLibrary from "@/components/CareerLibrary";
+
+export const metadata = {
+  title: "Career Library | HireLoop",
+  description:
+    "Practical guides on resumes, interviews, salary negotiation, and career growth.",
+};
+
+export default function CareerLibraryPage() {
+  return <CareerLibrary />;
+}
