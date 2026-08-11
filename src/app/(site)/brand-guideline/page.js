b8d510981b13797d397260b5ec4a0e0379243ea4
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-08-11 23:14:06 +0600

    created the /brand-guideline route page

---
+++ src/app/(site)/brand-guideline/page.js
@@ -0,0 +1,11 @@
+import BrandGuideline from "@/components/BrandGuideline";
+
+export const metadata = {
+  title: "Brand Guideline | HireLoop",
+  description:
+    "Logo, color, typography, and voice guidelines for the HireLoop brand.",
+};
+
+export default function BrandGuidelinePage() {
+  return <BrandGuideline />;
+}
