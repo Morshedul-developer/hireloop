3c6edc939ce5e648ac62198d80068a54d8901c55
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-08-12 20:33:27 +0600

    created the /newsroom route page

---
+++ src/app/(site)/newsroom/page.js
@@ -0,0 +1,11 @@
+import Newsroom from "@/components/Newsroom";
+
+export const metadata = {
+  title: "Newsroom | HireLoop",
+  description:
+    "Product launches, milestones, and press coverage from the HireLoop team.",
+};
+
+export default function NewsroomPage() {
+  return <Newsroom />;
+}
