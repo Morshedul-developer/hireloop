eff9225142b02b722fd1aeebe1629107e6bf474d
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-08-13 20:31:43 +0600

    created the /terms route page

---
+++ src/app/(site)/terms/page.js
@@ -0,0 +1,10 @@
+import Terms from "@/components/Terms";
+
+export const metadata = {
+  title: "Terms & Conditions | HireLoop",
+  description: "The terms and conditions for using the HireLoop platform.",
+};
+
+export default function TermsPage() {
+  return <Terms />;
+}
