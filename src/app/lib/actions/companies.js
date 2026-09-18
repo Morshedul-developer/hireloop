16b7712a3fba8f21c30c7562802a848703f9236d
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-09-18 17:41:57 +0600

    refactored the post api for jobs and companies

--- src/app/lib/actions/companies.js
+++ src/app/lib/actions/companies.js
@@ -1,14 +1,7 @@
 "use server";
 
-const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+import { serverMutation } from "../core/server";
 
 export const createCompany = async (payload) => {
-    const res = await fetch(`${baseUrl}/api/companies`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(payload)
-    });
-    return res.json();
-}
+  return serverMutation("/api/companies", payload);
+};
